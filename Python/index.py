86dc5ec48cb996f7207e5dd6529a82c6424b8602
Coralie Abbenzeller <coralie.abbenzeller@gmail.com>
2025-12-13 15:53:02 +0100

    WIP: Creating Python File, make some prints and working with some variables => First steps in python

---
+++ Python/index.py
@@ -0,0 +1,13 @@
+print('Hello World')
+print(57)
+minTemp=10
+maxTemp=25
+print("Minimum temperature was",minTemp,"degrees and the maximum was",maxTemp, "degrees")
+name='Coralie Sophie Abbenzeller'
+age=23
+height=1.65
+firm='I started my apprenticeship at Swissnet ICT GmbH'
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print(firm)
